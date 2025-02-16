016921a62b67013a851e2f574dac1a50a83ea11d
Vitali <shidlovskii2000@gmail.com>
2025-02-16 13:37:28 +0100

    fix(PENTA-001): otrefactoril todo

--- src/app/todo/components/todos/todos.component.ts
+++ src/app/todo/components/todos/todos.component.ts
@@ -16,11 +16,7 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.css',
 })
-export class TodosComponent implements OnInit, OnDestroy {
-  public todos$: Observable<Todo[]> = of([]);
-
-  public page = 1;
-
+export class TodosComponent {
   private todosArraySubject: BehaviorSubject<Todo[]> = new BehaviorSubject<
     Todo[]
   >([]);
@@ -34,58 +30,16 @@ export class TodosComponent implements OnInit, OnDestroy {
     switchMap((pageNumber) => this.todoService.getTodos(pageNumber)),
   );
 
-  public pageSubscription: Subscription | null = null;
-
   constructor(
     private todoService: TodoService,
     private destroyRef: DestroyRef,
   ) {}
 
-  ngOnInit(): void {
-    this.setTodos();
-    // this.getTodos();
-    // this.setPageSubscription();
-  }
-
-  ngOnDestroy(): void {
-    // this.pageSubscription?.unsubscribe();
-  }
-
   public nextPage(): void {
-    this.page = this.page + 1;
-    this.setTodos();
-    // this.getTodos();
     this.pageSubject.next(this.pageSubject.value + 1);
   }
 
   public previousPage(): void {
-    this.page = this.page - 1;
-    this.setTodos();
-    // this.getTodos();
     this.pageSubject.next(this.pageSubject.value - 1);
   }
-
-  private setTodos(): void {
-    this.todos$ = this.todoService.getTodos(this.page);
-  }
-
-  // public getTodos(): void {
-  //   this.todoService
-  //     .getTodos(this.page)
-  //     .subscribe((res: Todo[]) => this.todosArraySubject.next(res));
-  // }
-
-  // private setPageSubscription(): void {
-  //   // this.pageSubscription =
-  //   this.pageSubject
-  //     .pipe(
-  //       tap((res) => console.log(res)),
-  //       switchMap((pageNumber) => this.todoService.getTodos(pageNumber)), // return of([]) // of([]).subscribe => []
-  //       tap((res) => console.log(res)),
-  //       takeUntilDestroyed(this.destroyRef),
-  //     )
-  //     .subscribe((res: Todo[]) => {
-  //       this.todosArraySubject.next(res);
-  //     });
-  // }
 }
